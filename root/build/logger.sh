9313e4209ef218f9a4210495688a2b84a37c01e6
Fabien MARTY <fabien.marty@gmail.com>
2016-03-28 15:48:44 +0200

    use logger binary from upstream

--- root/build/logger.sh
+++ root/build/logger.sh
@@ -1,17 +1,6 @@
 #!/bin/bash
 
-VERSION=2.27.1
-ARCHIVE_NAME=v${VERSION}.tar.gz
-DIR_NAME=util-linux-${VERSION}
-ARCHIVE_URL=https://github.com/karelzak/util-linux/archive/${ARCHIVE_NAME}
-BUILD_DIR=/build
-
-wget -O ${BUILD_DIR}/${ARCHIVE_NAME} ${ARCHIVE_URL}
-cd ${BUILD_DIR} || exit 1
-zcat ${ARCHIVE_NAME} |tar xvf -
-cd ${DIR_NAME} || exit 1
-
-./autogen.sh
-./configure --prefix=/usr
-make logger
-cp logger /usr/bin/
+# We just copy the binary in /usr/local
+# /usr/bin/logger will we destroyed at the "remote build dependencies" 
+# phase
+cp -f /usr/bin/logger /usr/local/bin/logger
